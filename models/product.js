8ce914d4f0df34ee048099033775a415e787102f
damage786 <damage786@gmail.com>
2024-08-04 18:36:18 +0530

    updated

--- models/product.js
+++ models/product.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: String,
   category: String,
+  
   stock: Number,
   image: String,
   createdAt: { type: Date, default: Date.now }
